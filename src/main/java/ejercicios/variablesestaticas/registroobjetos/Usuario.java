0b1ac28b09cef82f747e5a8d1d760a9f4edc2fa4
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-03-17 18:44:42 +0100

    ejercicios variables estaticas

---
+++ src/main/java/ejercicios/variablesestaticas/registroobjetos/Usuario.java
@@ -0,0 +1,7 @@
+package ejercicios.variablesestaticas.registroobjetos;
+
+/**
+ * Crea una clase Usuario que tenga un atributo de instancia nombreUsuario y una variable estática usuariosRegistrados que almacene la cantidad de usuarios creados. Cada vez que se registre un nuevo usuario, la variable estática debe actualizarse.
+ */
+public class Usuario {
+}
---
+++ src/main/java/ejercicios/variablesestaticas/valoracumuladoobjetos/CuentaBancaria.java
@@ -0,0 +1,8 @@
+package ejercicios.variablesestaticas.valoracumuladoobjetos;
+
+/**
+ * Crea una clase CuentaBancaria que tenga un atributo de instancia saldo y un atributo estático saldoTotalBanco. Cada vez que se cree una nueva cuenta bancaria, su saldo inicial debe sumarse al saldo total del banco. Además, crea un método ingresarDinero(double cantidad) que permita ingresar dinero a la cuenta y actualizar saldoTotalBanco.
+ */
+
+public class CuentaBancaria {
+}
