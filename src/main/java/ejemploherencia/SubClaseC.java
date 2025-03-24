49438360b37eb4474552c085bf2759c321d84586
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-03-24 19:28:06 +0100

    primera clase herencia

---
+++ src/main/java/ejemploherencia/SubClaseC.java
@@ -0,0 +1,30 @@
+package ejemploherencia;
+
+public class SubClaseC extends SubClaseA{
+    private String atributoSubClaseC;
+
+    public SubClaseC(
+            String atributoSuperclase,
+            String atributoSubClaseA,
+            String atributoSubClaseC
+    ) {
+        super(atributoSuperclase, atributoSubClaseA);
+        this.atributoSubClaseC = atributoSubClaseC;
+    }
+
+    public String getAtributoSubClaseC() {
+        return atributoSubClaseC;
+    }
+
+    public void setAtributoSubClaseC(String atributoSubClaseC) {
+        this.atributoSubClaseC = atributoSubClaseC;
+    }
+
+    @Override
+    public String toString() {
+        return "SubClaseD{\n" +
+                "atributoSubClaseC='" + atributoSubClaseC + '\'' +
+                ",\natributoSubClaseA='" + atributoSubClaseA + '\'' +
+                ",\natributoSuperclase='" + atributoSuperclase + "\n}";
+    }
+}
---
+++ src/main/java/ejemploherencia/Superclase.java
@@ -0,0 +1,25 @@
+package ejemploherencia;
+
+public class Superclase {
+    protected String atributoSuperclase;
+
+    public Superclase(String atributoSuperclase) {
+        this.atributoSuperclase = atributoSuperclase;
+    }
+
+    public String getAtributoSuperclase() {
+
+        return atributoSuperclase;
+    }
+
+    public void setAtributoSuperclase(String atributoSuperclase) {
+        this.atributoSuperclase = atributoSuperclase;
+    }
+
+    @Override
+    public String toString() {
+        return "Superclase{" +
+                "atributoSuperclase='" + atributoSuperclase + '\'' +
+                '}';
+    }
+}
